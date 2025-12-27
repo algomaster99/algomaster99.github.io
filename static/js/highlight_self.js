004052cdddf36c84f0a3754c742bcde7748f1588
Aman Sharma <mannu.poski10@gmail.com>
2025-12-27 18:24:44 +0100

    Highlight myself without JS

--- static/js/highlight_self.js
+++
@@ -1,8 +0,0 @@
-window.addEventListener("DOMContentLoaded", (event) => {
-    const authors = document.getElementsByClassName("author");
-    for (let i = 0; i < authors.length; i++) {
-        if (authors[i].innerText === "Aman Sharma") {
-            authors[i].classList.add("highlight");
-        }
-    }
-});
